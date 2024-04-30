deaffcc0fe6a9c93775e1a2568336b039bea12d0
하송희 <99heesong@naver.com>
2024-05-01 03:18:41 +0900

    기능추가: editor 영역 크기 수정, 페이지 목록 리스트 제목 줄이기

--- src/components/Editor/EditorPage.js
+++ src/components/Editor/EditorPage.js
@@ -12,8 +12,8 @@ export default function EditorPage({ $target, initialState, onEdit }) {
 
   this.render = () => {
     $editorPage.innerHTML = `
-      <input type="text" name="title" placeholder="제목을 입력하세요 😚" ></input>
-      <textarea name="content" placeholder="내용을 입력해보세요 🧚🏻‍♀️"></textarea>
+      <input type="text" name="title" placeholder="제목을 입력하세요 😚" spellcheck="false"></input>
+      <textarea name="content" placeholder="내용을 입력하세요 🧚🏻‍♀️" spellcheck="false"></textarea>
     `;
   };
 
